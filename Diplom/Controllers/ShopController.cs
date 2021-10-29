365e403cb38d5e255e2a7a3e50cf764e368b7336
denchikj <igoshevdenis9876@gmail.com>
2021-10-29 14:29:52 +0500

    Добавлена модель, доработать каталог товаров!

--- Diplom/Controllers/ShopController.cs
+++ Diplom/Controllers/ShopController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Diplom.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +12,25 @@ namespace Diplom.Controllers
 {
     public class ShopController : Controller
     {
+        private Entities db = new Entities();
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            return View(db.Product.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
         }
     }
 }
